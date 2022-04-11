66a1d398f8bd4419363361eb9cb918ea97840730
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2022-04-11 10:12:55 +0200

    Create 2_result_imputation.R

---
+++ R/2_result_imputation.R
@@ -0,0 +1,100 @@
+
+library(tidyverse)
+library(RColorBrewer)
+
+
+# Raw data ---------------------------
+# Ruta de la consola y guardar en variable
+ruta_excel <- "raw_data/DatosTFG.xlsx"
+
+# Hojas del Excel
+excel_sheets(ruta_excel)
+
+# Importar los datos de la Hoja Exterior
+dato_exterior <- read_excel(ruta_excel, sheet = "Exterior")
+
+# Pasar a numerico la Temperatura exterior
+dato_exterior$Temperatura <- suppressWarnings(as.numeric(dato_exterior$Temperatura))
+dato_exterior$Humedad <- suppressWarnings(as.numeric(dato_exterior$Humedad))
+
+#Obtener los datos de tiempo
+x <- dato_exterior$FechaNum
+
+
+# Processed data ---------------------------
+Hum_imp_aux <- read_csv("processed_data/ImputacionDatosHum.csv")
+Temp_imp_aux <- read_csv("processed_data/ImputacionDatosTem.csv")
+MF_imp <- read_csv("processed_data/ImputacionDatosMF.csv")
+
+Temp_MF_imp <- MF_imp %>% select(fecha_num,Tem_missForest) %>% rename(missForest = Tem_missForest)
+Hum_MF_imp <- MF_imp %>% select(fecha_num,Hum_missForest) %>% rename(missForest = Hum_missForest)
+
+Hum_imp <- Hum_imp_aux %>% left_join(Hum_MF_imp, by = "fecha_num")
+Temp_imp <- Temp_imp_aux %>% left_join(Temp_MF_imp, by = "fecha_num")
+
+Hum_imp_final <- Hum_imp %>% gather("imputation_technique", "value", -fecha_num)
+Temp_imp_final <- Temp_imp %>% gather("imputation_technique", "value", -fecha_num)
+
+
+plot_imputation_hum <- ggplot(Hum_imp_final)+
+  geom_line(aes(x = fecha_num, y = value, color = imputation_technique), size = 1)+
+  scale_color_brewer(palette = "Paired")+
+  facet_wrap(~imputation_technique)+
+  geom_line(data = dato_exterior, aes(x = FechaNum, y = Humedad), size = 1)+
+  theme_bw()
+
+plot_imputation_temp <- ggplot(Temp_imp_final)+
+  geom_line(aes(x = fecha_num, y = value, color = imputation_technique), size = 1)+
+  scale_color_brewer(palette = "Paired")+
+  facet_wrap(~imputation_technique)+
+  geom_line(data = dato_exterior, aes(x = FechaNum, y = Temperatura), size = 1)+
+  theme_bw()
+  
+
+plot_imputation_hum_NN_RF <- ggplot(Hum_imp_final %>% 
+                                      filter(imputation_technique %in% c("IMNNI","missForest")))+
+  geom_line(aes(x = fecha_num, y = value, color = imputation_technique), size = 1)+
+  scale_color_brewer(palette = "Paired")+
+  facet_wrap(~imputation_technique, ncol = 1)+
+  geom_line(data = dato_exterior, aes(x = FechaNum, y = Humedad), size = 1)+
+  theme_bw()
+
+plot_imputation_temp_NN_RF <- ggplot(Temp_imp_final%>% 
+                                       filter(imputation_technique %in% c("IMNNI","missForest")))+
+  geom_line(aes(x = fecha_num, y = value, color = imputation_technique), size = 1)+
+  scale_color_brewer(palette = "Paired")+
+  facet_wrap(~imputation_technique, ncol = 1)+
+  geom_line(data = dato_exterior, aes(x = FechaNum, y = Temperatura), size = 1)+
+  theme_bw()
+
+
+
+
+png("figures/imputation_temperature.png",
+    width = 11.69, # The width of the plot in inches
+    height = 11.69*0.5, units = "in", res=300*2)
+plot_imputation_temp
+# plot_annotation(tag_levels = c("I", "a"))
+dev.off()
+
+png("figures/imputation_humidity.png",
+    width = 11.69, # The width of the plot in inches
+    height = 11.69*0.5, units = "in", res=300*2)
+plot_imputation_hum
+# plot_annotation(tag_levels = c("I", "a"))
+dev.off()
+
+png("figures/imputation_temperature_NN_RF.png",
+    width = 11.69, # The width of the plot in inches
+    height = 11.69*0.5, units = "in", res=300*2)
+plot_imputation_temp_NN_RF
+# plot_annotation(tag_levels = c("I", "a"))
+dev.off()
+
+png("figures/imputation_humidity_NN_RF.png",
+    width = 11.69, # The width of the plot in inches
+    height = 11.69*0.5, units = "in", res=300*2)
+plot_imputation_hum_NN_RF
+# plot_annotation(tag_levels = c("I", "a"))
+dev.off()
+
